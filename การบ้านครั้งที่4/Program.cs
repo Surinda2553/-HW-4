93f9deade179c7debaf8bf7fd8c4658f2445b725
Surinda Sarakum <surindasarakum18@gmail.com>
2021-04-18 16:25:22 +0700

    Add project files.

---
+++ การบ้านครั้งที่4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace การบ้านครั้งที่4
+{
+	public class Program
+	{
+		static void printPascal(int n)
+		{
+			n++;
+			for (int line = 0; line < n; line++)
+			{
+				for (int i = 0; i <= line; i++)
+					Console.Write(binomialCoeff(line, i) + " ");
+				Console.WriteLine();
+			}
+		}
+
+		static int binomialCoeff(int n, int k)
+		{
+			int res = 1;
+			if (k > n - k)
+				k = n - k;
+			for (int i = 0; i < k; ++i)
+			{
+				res *= (n - i);
+				res /= (i + 1);
+			}
+
+			return res;
+		}
+
+		public static void Main(string[] args)
+		{
+			int n = Convert.ToInt32(Console.ReadLine());
+			if (n < 0)
+			{
+				while (n < 0)
+				{
+					Console.WriteLine("Invalid Pascal's triangle row number.\n");
+					n = Convert.ToInt32(Console.ReadLine());
+				}
+			}
+
+			printPascal(n);
+		}
+	}
+}
